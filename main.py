c3265e43b0110af8516cc22f0258c68d313793ca
Mastercelta <juancamilo@bees.com.co>
2025-04-01 00:34:11 -0500

    Agregar componentes

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
-from reactpy import component, html, run
+import asyncio
+from reactpy import component, web, html, run
 from reactpy.backend.flask import configure
 from flask import Flask
 from datos import productos
 
+mui = web.module_from_template("react", "@mui/material", fallback="")
+Container = web.export(mui, "Container")
+Grid = web.export(mui, "Grid")
+Paper = web.export(mui, "Paper")
+
+
+def tarjetas():
+    def tarjeta(producto):
+        return Grid(
+            {"item": True, "sm": 6, "md": 4, "lg": 3},
+            Paper(
+                {"elevation": 4},
+            ),
+            html.h1(producto["nombre"]),
+        )
+
+    return Grid(
+        {"container": True, "spacing": "8"},
+        *[tarjeta(producto) for producto in productos],
+    )
+
 
 @component
 def App():
-    return html.div(
-        html.h1("Hola Mundo"),
-        html.h2("Lista de Productos"),
-        html.ul([html.li(producto["nombre"]) for producto in productos]),
+    return Container(
+        {"maxWidth": "md"},
+        Grid(
+            {"container": True, "spacing": 2},
+            tarjetas(),
+        ),
     )
 
 
 app = Flask(__name__)
-configure(app, App)  # Configura la aplicación Flask para usar ReactPy
-app.run(host="0.0.0.0", debug=True)  # Ejecuta la aplicación Flask en el puerto 5000
+configure(app, App)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)  # Ejecuta la aplicación Flask en el puerto 5000
